0052da5643f7fe7446c864f27622ff5daa45318c
JeffWang <jeffwang987@163.com>
2019-12-18 22:50:09 +0800

    给task1增加了注释，因为要交作业

--- task1/task1.py
+++ task1/task1.py
@@ -14,34 +14,43 @@ import numpy as np
 import time
 
 """给原图添加噪声"""
-image = cv2.imread("Lenna.jpg")
-utils = imutils.Utils()
-gray = utils.rgb2gray(image)
-blur_sp = utils.sp_noise(gray, prob=0.01)
+image = cv2.imread("Lenna.jpg")  # 读取图像
+utils = imutils.Utils()  # 对象实例化，这个用于RGB转灰度、增加噪声等
+gray = utils.rgb2gray(image)  # 转为灰度图像
+blur_sp = utils.sp_noise(gray, prob=0.01)  # 添加1%的椒盐噪声
 
 """执行快速中值滤波算法"""
-start = time.time()
-output = np.copy(blur_sp)
-kernel_size = (3, 3)
-task1 = imutils.FastMedianBlur(blur_sp, kernel_size)
-for i in range(blur_sp.shape[0]-kernel_size[0]):
+start = time.time()  # 算法开始计时
+output = np.copy(blur_sp)  # 先为输出图像创建空间
+kernel_size = (3, 3)  # 我们的中值滤波模板
+task1 = imutils.FastMedianBlur(blur_sp, kernel_size)  # 对象实例化，这个用于快速中值滤波算法
+for i in range(blur_sp.shape[0]-kernel_size[0]):  # 行循环
+    # 每次到一个新行的时候，要把直方图清零
     task1.hist2zero()
+    # 每一行第一个window的截取
     window_start = blur_sp[i:i+kernel_size[0], 0:kernel_size[1]]
+    # 计算每一行第一个window的直方图
     task1.calhist(window_start)
+    # 利用希尔排序求每一行第一个window的中值
     task1.findmedian(window_start)
-    for j in range(blur_sp.shape[1]-kernel_size[1]):
+    for j in range(blur_sp.shape[1]-kernel_size[1]):  # 列循环
+        # 因为每一行第一个求过了，可以跳过
         if j == 0:
             median = task1.get_median()
             output[i][j] = median
             continue
+        # 截取window
         window = blur_sp[i:i+kernel_size[0], j:j+kernel_size[1]]
+        # 按照论文方法更新直方图
         task1.update(window)
+        # 按照论文方法更新中值
         median = task1.get_median()
+        # 把中值代替原来的像素灰度
         output[i][j] = median
 
-end = time.time()
-print("time consumed：{:.4f}".format(end-start))
-cv2.imshow("Lenna", image)
+end = time.time()  # 算法开始计时
+print("time consumed：{:.4f}".format(end-start))  # 显示算法用时
+cv2.imshow("Original", image)  # 显示对比原图、噪声图、滤波图
 cv2.imshow("Spice salt noise image", blur_sp)
 cv2.imshow("output", output)
 cv2.waitKey(0)
